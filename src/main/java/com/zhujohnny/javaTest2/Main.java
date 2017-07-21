bc0cc66e03b001123c065b604fb87164e26673eb
mk <zhu.johnny@mkcorp.com>
2017-07-21 16:16:31 +0800

    add a comment for test jenkins

--- src/main/java/com/zhujohnny/javaTest2/Main.java
+++ src/main/java/com/zhujohnny/javaTest2/Main.java
@@ -8,5 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("this is a java test2 demo...");
+
+        System.out.println("add a step for test jenkins...");
     }
 }
